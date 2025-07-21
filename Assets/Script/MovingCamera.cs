10b4ed1fc010e197e598e2819fe5f55e7c30bed8
Yoon <panopticon0847@gmail.com>
2025-07-21 21:56:07 +0900

    feat : 충돌 수정

--- Assets/Script/MovingCamera.cs
+++ Assets/Script/MovingCamera.cs
@@ -4,21 +4,23 @@ using static UnityEngine.GraphicsBuffer;
 public class MovingCamera : MonoBehaviour
 {
 
-    public Transform target;
 
-    public Vector3 offset = new Vector3(0, 1.6f, -4f);
-
-    public float followSpeed = 5f;
-    public float rotatedSpeed = 3f;
+    public GameObject target;
+ 
+    public float rotateSpeed = 3f;
+    public Vector3 offset = new Vector3(0, 5, -5);
 
     private void LateUpdate()
     {
-        Vector3 desiredPos = target.position + target.forward * offset.z + Vector3.up * offset.y;
-        transform.position = Vector3.Lerp(transform.position, desiredPos, followSpeed * Time.deltaTime);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation,
-            Quaternion.LookRotation(target.forward),
-            rotatedSpeed * Time.deltaTime);
+
+        Quaternion targetRotation = Quaternion.Euler(0, target.transform.eulerAngles.y, 0);
+        Vector3 desiredPosition = target.transform.position + targetRotation * offset;
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, rotateSpeed * Time.deltaTime);
+
+        transform.LookAt(target.transform.position + Vector3.up * 1.5f);
+
     }
 
 }
